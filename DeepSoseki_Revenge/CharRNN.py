7d91780dc91c7e053652d01c9489893a6c5cb7e0
DoliDoli <basrooom@gmail.com>
2016-08-01 11:48:20 +0900

    結果にこみっと！

--- DeepSoseki_Revenge/CharRNN.py
+++ DeepSoseki_Revenge/CharRNN.py
@@ -66,3 +66,31 @@ def make_initial_state(n_units, batchsize=50, train=True):
     return {name: Variable(np.zeros((batchsize, n_units), dtype=np.float32),
             volatile=not train)
             for name in ('c1', 'h1', 'c2', 'h2')}
+
+
+# 新しい書き方でのRNNの定義の仕方
+"""
+class RNNForLM(chainer.Chain):
+
+    def __init__(self, n_vocab, n_units, train=True):
+        super(RNNForLM, self).__init__(
+            embed=L.EmbedID(n_vocab, n_units),
+            l1=L.LSTM(n_units, n_units),
+            l2=L.LSTM(n_units, n_units),
+            l3=L.Linear(n_units, n_vocab),
+        )
+        for param in self.params():
+            param.data[...] = np.random.uniform(-0.1, 0.1, param.data.shape)
+        self.train = train
+
+    def reset_state(self):
+        self.l1.reset_state()
+        self.l2.reset_state()
+
+    def __call__(self, x):
+        h0 = self.embed(x)
+        h1 = self.l1(F.dropout(h0, train=self.train))
+        h2 = self.l2(F.dropout(h1, train=self.train))
+        y = self.l3(F.dropout(h2, train=self.train))
+        return y
+"""
